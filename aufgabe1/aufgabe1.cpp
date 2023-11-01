ad6fcd009875ae4b89706401b8c171c05763bc3d
Finn Thierolf <finnt02@zedat.fu-berlin.de>
2023-11-01 20:07:33 +0100

    first commit

---
+++ aufgabe1/aufgabe1.cpp
@@ -0,0 +1,23 @@
+/*
+Finn Thierolf
+01.11.2023
+aufgabe1.cpp
+*/
+
+#include "aufgabe1.h"
+#include <iostream>
+
+void Horspool::setPattern(const std::string& pat)
+{
+
+}
+
+const std::string& Horspool::getPattern() const
+{
+
+}
+
+std::vector<size_t> Horspool::getHits(const std::string& text) const
+{
+    
+}
