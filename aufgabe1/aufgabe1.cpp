9849ead56dfa92a904d64068b4394f170f6fc392
Finn Thierolf <finnt02@zedat.fu-berlin.de>
2023-11-01 22:05:45 +0100

    imp setPattern, getPattern

--- aufgabe1/aufgabe1.cpp
+++ aufgabe1/aufgabe1.cpp
@@ -9,15 +9,32 @@ aufgabe1.cpp
 
 void Horspool::setPattern(const std::string& pat)
 {
+    Horspool::pattern = pat;
 
+    uint8_t length = pat.size();
+    for (uint8_t position = 0; position < (length-1); position++)
+    {
+        Horspool::lookuptab[pat[position]] = length - position;
+    }
+
+    if (pat[length] != pat[length-1])
+    {
+        Horspool::lookuptab.erase(length);
+    }
+    Horspool::lookuptab['*'] = length;
 }
 
 const std::string& Horspool::getPattern() const
 {
-
+    return Horspool::pattern;
 }
 
 std::vector<size_t> Horspool::getHits(const std::string& text) const
 {
-    
+    std::string mask = {};
+    uint8_t length = Horspool::pattern.size();
+    for (uint8_t begin = 0; begin < text.size() - length; )
+    {
+        mask = text.substr(begin, begin+length);
+    }
 }
