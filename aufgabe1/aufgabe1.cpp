1b0ab4aea1fb209a30f358878c649eed25b2b0ff
Finn Thierolf <finnt02@zedat.fu-berlin.de>
2023-11-04 16:40:49 +0100

    1. Versuch allignCheck_

--- aufgabe1/aufgabe1.cpp
+++ aufgabe1/aufgabe1.cpp
@@ -27,51 +27,27 @@ const std::string& Horspool::getPattern() const
 std::vector<size_t> Horspool::getHits(const std::string& text) const
 {
     std::vector<size_t> hits;
-    hits.resize(0);
-    size_t pos = 0;
-    while (pos <= text.size() - Horspool::pattern.size())
+    size_t text_pos = 0;
+    while (text_pos <= text.size() - Horspool::pattern.size())
     {
         size_t maskpos = Horspool::pattern.size() - 1;
-        while (maskpos > 0 && text[pos+maskpos] == Horspool::pattern[maskpos])
+        while ((maskpos > 0 && text[text_pos+maskpos] == Horspool::pattern[maskpos]) || (maskpos > 0 && Horspool::pattern[maskpos] == '?') || (maskpos > 0 && text[text_pos+maskpos] == '?'))
         {
-            Horspool::alignCheck_(pos+maskpos);
             maskpos--;
         }
         
-        if (maskpos == 0)
+        if ((maskpos == 0 && text[text_pos] == Horspool::pattern[0]) || (maskpos == 0 && Horspool::pattern[maskpos] == '?') || (maskpos == 0 && text[text_pos+maskpos] == '?'))
         {
-            Horspool::alignCheck_(pos);
-            hits.push_back(pos+1);
+            hits.push_back(text_pos);
         }
-
-        pos += Horspool::getShift_(text[pos+Horspool::pattern.size()]);
+        
+        Horspool::alignCheck_(text_pos);
+        text_pos += Horspool::getShift_(text[text_pos+Horspool::pattern.size()-1]);
     }
     return hits;
 }
 
 
-/*
-std::vector<size_t> Horspool::getHits(const std::string& text) const {
-    std::vector<size_t> hits;
-    size_t text_len = text.size();
-    size_t pattern_len = Horspool::pattern.size();
-
-    size_t i = pattern_len - 1;
-    while (i < text_len) {
-        size_t k = 0;
-        while (k < pattern_len && Horspool::pattern[pattern_len - 1 - k] == text[i - k]) {
-            k++;
-        }
-        if (k == pattern_len) {
-            hits.push_back(i - pattern_len + 1);
-        }
-        Horspool::alignCheck_(i);
-        i += getShift_(text[i]);
-    }
-
-    return hits;
-}
-*/
 uint32_t Horspool::getShift_(const char last_char) const
 {
     auto it = Horspool::lookuptab.find(last_char);
