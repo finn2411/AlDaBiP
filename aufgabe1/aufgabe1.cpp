5d6cd3a687c9d951e5cbb360e1a2d6f01ac21e0f
Finn Thierolf <finnt02@zedat.fu-berlin.de>
2023-11-04 16:49:04 +0100

    formating

--- aufgabe1/aufgabe1.cpp
+++ aufgabe1/aufgabe1.cpp
@@ -7,46 +7,44 @@ aufgabe1.cpp
 #include "aufgabe1.h"
 #include <iostream>
 
-void Horspool::setPattern(const std::string& pat)
+void Horspool::setPattern(const std::string &pat)
 {
     Horspool::pattern = pat;
 
     uint32_t length = pat.size();
-    for (uint32_t position = 0; position < (length-1); position++)
+    for (uint32_t position = 0; position < (length - 1); position++)
     {
         Horspool::lookuptab[pat[position]] = length - position - 1;
     }
 }
 
-const std::string& Horspool::getPattern() const
+const std::string &Horspool::getPattern() const
 {
     return Horspool::pattern;
 }
 
-
-std::vector<size_t> Horspool::getHits(const std::string& text) const
+std::vector<size_t> Horspool::getHits(const std::string &text) const
 {
     std::vector<size_t> hits;
     size_t text_pos = 0;
     while (text_pos <= text.size() - Horspool::pattern.size())
     {
         size_t maskpos = Horspool::pattern.size() - 1;
-        while ((maskpos > 0 && text[text_pos+maskpos] == Horspool::pattern[maskpos]) || (maskpos > 0 && Horspool::pattern[maskpos] == '?') || (maskpos > 0 && text[text_pos+maskpos] == '?'))
+        while ((maskpos > 0 && text[text_pos + maskpos] == Horspool::pattern[maskpos]) || (maskpos > 0 && Horspool::pattern[maskpos] == '?') || (maskpos > 0 && text[text_pos + maskpos] == '?'))
         {
             maskpos--;
         }
-        
-        if ((maskpos == 0 && text[text_pos] == Horspool::pattern[0]) || (maskpos == 0 && Horspool::pattern[maskpos] == '?') || (maskpos == 0 && text[text_pos+maskpos] == '?'))
+
+        if ((maskpos == 0 && text[text_pos] == Horspool::pattern[0]) || (maskpos == 0 && Horspool::pattern[maskpos] == '?') || (maskpos == 0 && text[text_pos + maskpos] == '?'))
         {
             hits.push_back(text_pos);
         }
-        
+
         Horspool::alignCheck_(text_pos);
-        text_pos += Horspool::getShift_(text[text_pos+Horspool::pattern.size()-1]);
+        text_pos += Horspool::getShift_(text[text_pos + Horspool::pattern.size() - 1]);
     }
     return hits;
 }
-
 
 uint32_t Horspool::getShift_(const char last_char) const
 {
