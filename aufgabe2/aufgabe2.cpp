3edea35dd57572740800b7d2c2e4d001e735430b
Finn Thierolf <finnt02@zedat.fu-berlin.de>
2023-11-09 19:59:59 +0100

    function 'construct()' done and working in main

--- aufgabe2/aufgabe2.cpp
+++ aufgabe2/aufgabe2.cpp
@@ -1,21 +1,35 @@
 /*
-
-
+    Mobarak Almoustafa
+    Finn Thierolf
+    AlDaBi Praktikum
+    Aufgabe 3 (.cpp)
 */
 
 #include <iostream>
+#include <algorithm>
+#include <numeric>
 #include "aufgabe2.hpp"
 
 
 void construct(std::vector<uint32_t>& sa, const std::string& text)
 {
-
-}
-
-
-
-int main()
-{
-
+    // reset suffix array to new length
+    sa.clear();
+    sa.resize(text.size());
+    std::iota(sa.begin(), sa.end(), 0);
     
+    // defining and executing sort function
+    std::sort(sa.begin(), sa.end(), [text] (const uint32_t &a, const uint32_t &b)
+    {
+        // if first letter of a is "smaller" or, if the first letters are the same, if the substring
+        // from this position on is shorter a is "smaller" than b
+        if (text[a] < text[b] || (text[a] == text[b] && a > b))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    });
 }
--- aufgabe2/aufgabe2_main.cpp
+++ aufgabe2/aufgabe2_main.cpp
@@ -1,6 +1,31 @@
+/*
+    Mobarak Almoustafa
+    Finn Thierolf
+    AlDaBi Praktikum
+    Aufgabe 3 (main)
+*/
+
 #include <iostream>
+#include "aufgabe2.hpp"
 
 int main(int argc, const char* argv[])
 {
-    
+    if (argc < 2)
+    {
+        std::cout << "suffix array: " << argv[0] << " <TEXT>" << std::endl;
+        std::cout << "for binary search: " << argv[0] << " <TEXT> <SEARCHWORD1> <SEARCHWORD2> ..." << std::endl;
+        return 1;
+    }
+
+    std::vector<uint32_t> suffixArray = {};
+
+    if (argc < 3)
+    {
+        construct(suffixArray, argv[1]);
+        for (const uint32_t element : suffixArray)
+        {
+            std::cout << element << std::endl;
+        }
+    }
+
 }
