6d97d997bc4f1d9e0e7b19061480731af9c1f2d6
Finn Thierolf <finnt02@zedat.fu-berlin.de>
2023-11-30 20:47:17 +0100

    finish main

--- aufgabe3/aufgabe3_main.cpp
+++ aufgabe3/aufgabe3_main.cpp
@@ -8,14 +8,40 @@ aufgabe1.cpp
 #include<iostream>
 #include"Alignment.hpp"
 
-int main()
+int main(int argc, const char* argv[])
 {
-    std::string a1;
-    std::string gaps;
-    std::string a2;
-    Alignment test ("IMISSMISSISSIPPI", "MYMISSISAHIPPIE");
-    test.compute(3, -4, -6);
-    std::cout << test.getScore() << std::endl;
-    test.getAlignment(a1, gaps, a2);
-    std::cout << a1 << std::endl << gaps << std::endl << a2 << std::endl;
+    if (argc == 3)
+    {                   
+        std::string a1;
+        std::string gaps;
+        std::string a2;
+        Alignment align (argv[1], argv[2]);
+        align.compute(3, -1, -2);
+        align.getAlignment(a1, gaps, a2);
+        std::cout << a1 << std::endl;
+        std::cout << gaps << std::endl;
+        std::cout << a2 << std::endl;
+        std::cout << "score: " << align.getScore() << std::endl;
+    }
+
+    else if (argc == 6)
+    {
+        std::string a1;
+        std::string gaps;
+        std::string a2;
+        Alignment align (argv[1], argv[2]);
+        align.compute(std::stoi(argv[3]), std::stoi(argv[4]), std::stoi(argv[5]));
+        align.getAlignment(a1, gaps, a2);
+        std::cout << a1 << std::endl;
+        std::cout << gaps << std::endl;
+        std::cout << a2 << std::endl;
+        std::cout << "score: " << align.getScore() << std::endl;
+    }
+
+    else
+    {
+        std::cout << "unexpected input"<< std::endl;
+        return 1;
+    }
+    
 }
