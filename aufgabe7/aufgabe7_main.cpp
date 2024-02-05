1c939ddd2902ca0e09512c766a54837ef63e13cc
maxid04 <maxim.daniel@fu-berlin.de>
2024-02-05 16:25:44 +0100

    main: klappt mit allen Beispielen!

--- aufgabe7/aufgabe7_main.cpp
+++ aufgabe7/aufgabe7_main.cpp
@@ -8,25 +8,51 @@ AlDaBi aufgabe7
 
 #include "PDA.hpp"
 #include <iostream>
+#include <algorithm>
 
 int main(int argc, const char *argv[])
 {
-    PDA::Language l = PDA::Language::BRACKETS;
-    PDA test(l);
-    std::string query = "(()";
-
-    PDA::State temp{};
-    for (char character : query)
+    if(argc==2)
     {
-        temp = test.next(character);
-        if(temp==PDA::State::FAIL) std::cout<< "FAIL\n";
+        //saving sequence in variable
+        std::string seq= argv[1];
 
+        //setting language as stabndard (HAIRPIN)
+        PDA::Language l=PDA::Language::HAIRPIN;
+        //checking if sequence has language BRACKETS
+        if(seq.find('(')!=std::string::npos || seq.find(')')!=std::string::npos)
+        {
+            std::string tempSeq{};
+            l=PDA::Language::BRACKETS;
+
+            //filtering non-bracket symbols  + #include <algorithm>
+            seq.erase(std::remove_if(seq.begin(), seq.end(), [](char c) {
+                return c != '(' && c != ')';
+            }), seq.end());
+        }
+        std::cout << seq <<std::endl;
+        PDA automat(l);
+
+        //putting '$' on the end of the string!
+        seq+='$'; 
+        PDA::State curState=PDA::State::IN_PROGRESS;
+
+        for(char c : seq)
+        {
+            curState=automat.next(c);
+            if(curState==PDA::State::FAIL) break;
+        }
+        
+        if(curState==PDA::State::FAIL || curState==PDA::State::IN_PROGRESS)
+        {
+            std::cout << "FAIL\n";
+            return 0;
+        }
+        else
+        {
+            std::cout << "ACCEPT\n";
+            return 1;
+        }
     }
-
- 
-    if(temp == PDA::State::IN_PROGRESS) temp = test.next('$');
-
-    if(temp == PDA::State::FAIL) std::cout << "Fail!!!\n";
-    
-    else if(temp==PDA::State::SUCCESS) std::cout<< "SUCCESS\n";
+    else throw std::logic_error("Expected different Commandline arguments! E.g.: ./aufgabe7_main gccgcaaggc");
 }
